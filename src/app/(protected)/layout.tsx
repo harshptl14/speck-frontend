1fa317ab40006b875154058f8d800daf02ea9dfb
harshptl14 <hi.arsh07@gmail.com>
2024-09-02 14:08:20 -0400

    chore: Add @tailwindcss/typography plugin for markdown styling
    - changed tailwind config for tailwindcss/typography

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,37 +1,4 @@
 import type { Metadata } from "next";
-import Link from "next/link";
-import {
-  Bell,
-  CircleUser,
-  Home,
-  LineChart,
-  Menu,
-  Package,
-  Package2,
-  Search,
-  ShoppingCart,
-  Users,
-} from "lucide-react";
-
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import SideNav from "@/components/sideNav";
 import MainNav from "@/components/mainNav";
 
