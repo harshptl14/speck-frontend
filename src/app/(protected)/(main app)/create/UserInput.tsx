3e909550b5a19dc5d0442a7a7e064a2b11627571
Harsh Patel <hi.arsh07@gmail.com>
2024-11-21 16:01:32 +0530

    Update UserInput.tsx

--- src/app/(protected)/(main app)/create/UserInput.tsx
+++ src/app/(protected)/(main app)/create/UserInput.tsx
@@ -139,7 +139,7 @@ export default function RoadmapCreationComponent() {
       .find((cookie) => cookie.includes("jwtToken"))
       ?.split("=")[1];
 
-    const newSocket = io(process.env.NEXT_PUBLIC_SOCKET_API!, {
+    const newSocket = io(process.env.NEXT_PUBLIC_SOCKET_API, {
       auth: { token: `Bearer ${authorization}` },
       withCredentials: true,
     });
